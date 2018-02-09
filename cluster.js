54575c6350a54689883f0151b7af4039810751cd
ctmurphy77 <rajonwitness@gmail.com>
2018-02-09 18:02:02 -0500

    Finally fixed cluster and domain code

--- cluster.js
+++ cluster.js
@@ -6,23 +6,23 @@ function startWorker() {
 }
 
 if(cluster.isMaster){
-   require('os').cpus().forEach(function(){
-   startWorker();
-  });
+    require('os').cpus().forEach(function(){
+        startWorker();
+    });
 
-  // log any workers that disconnect; if a worker disconnects, it
-  // should then exit, so we'll wait for the exit event to spawn
-  // a new worker to replace it
-  cluster.on('disconnect', function(worker){
-    console.log('CLUSTER: Worker %d disconnected from the cluster.', worker.id);
-  });
+    // log any workers that disconnect; if a worker disconnects, it
+    // should then exit, so we'll wait for the exit event to spawn
+    // a new worker to replace it
+    cluster.on('disconnect', function(worker){
+        console.log('CLUSTER: Worker %d disconnected from the cluster.', worker.id);
+    });
 
-  // when a worker dies (exits), create a worker to replace it
-  cluster.on('exit', function(worker, code, signal){
-    console.log('CLUSTER: Worker %d died with exit code %d (%s)', worker.id, code, signal);
-    startWorker();
-  });
+    // when a worker dies (exits), create a worker to replace it
+    cluster.on('exit', function(worker, code, signal){
+        console.log('CLUSTER: Worker %d died with exit code %d (%s)', worker.id, code, signal);
+        startWorker();
+    });
 } else {
-   // start our app on worker; see meadowlark.js
-   require('./index.js')();
+    // start our app on worker; see meadowlark.js
+    require('./index.js')();
 }
--- views/tours/meadowlark.js
+++ views/tours/meadowlark.js
@@ -1,6 +1,6 @@
 app.get('/tours/hood-river', function(req, res){
- res.render('tours/hood-river');
+    res.render('tours/hood-river');
 });
 app.get('/tours/request-group-rate', function(req, res){
- res.render('tours/request-group-rate');
+    res.render('tours/request-group-rate');
 });
